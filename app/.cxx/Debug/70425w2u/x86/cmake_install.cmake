ff094f2a729649efe2ec53a7295fa9bec6865b92
Georg <selftitled1@gmail.com>
2025-08-04 02:21:35 +0300

    adapting project for transfer between PCs

--- app/.cxx/Debug/70425w2u/x86/cmake_install.cmake
+++ app/.cxx/Debug/70425w2u/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/georg/Documents/Android_Audio_App/Android_Audio_Workstation/app/src/main/cpp
+# Install script for directory: C:/Users/selft/Android_Audio_Workstation/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/georg/AppData/Local/Android/Sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/selft/AppData/Local/Android/Sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/georg/Documents/Android_Audio_App/Android_Audio_Workstation/app/.cxx/Debug/70425w2u/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/selft/Android_Audio_Workstation/app/.cxx/Debug/70425w2u/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
